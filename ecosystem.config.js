334a26e14a8a85463a929c818fa4e600509af09f
Luca Provencal <lucapro3@yahoo.com>
2019-05-10 18:54:56 -0400

    add pm2 deploy script

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'thedrumroom',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-234-30-106.compute-1.amazonaws.com',
+      key: '~/.ssh/TheDrumRoomSSHKey.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:LucaProvencal/EC2-Deployment.git',
+      path: '/home/ubuntu/EC2-Deployment',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
